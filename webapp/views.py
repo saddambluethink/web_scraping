34af5edc72ab5f3c80d8604afa24eba4e490f1e3
saddam <saddambluethink@gmail.com>
2021-11-16 20:12:12 +0530

    amazon_web_scraping

--- webapp/views.py
+++ webapp/views.py
@@ -4,6 +4,7 @@ from selenium import webdriver  #install webdriver-manager
 from selenium.webdriver.common.keys import Keys  
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 # Create your views here.
 
 
@@ -58,20 +59,59 @@ def webscrap(request):
 
 
 def amazon(request):
-    driver = webdriver.Chrome(ChromeDriverManager().install()) 
-    url="https://www.amazon.in/"    
-    driver.get(url) 
-    time.sleep(2)
-    driver.find_element_by_id('twotabsearchtextbox').send_keys("apple mobile" + Keys.RETURN)
+    if request.method=='POST':
+        sr=request.POST['data']
+        driver = webdriver.Chrome(ChromeDriverManager().install()) 
+        url="https://www.amazon.in/" 
+        driver.get(url)
+        
+        driver.find_element_by_id('twotabsearchtextbox').send_keys(sr)
+        time.sleep(5)
+        driver.find_element_by_xpath('/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[3]/div/span/input').click()
+        time.sleep(15)
+    
+        data = driver.find_elements_by_class_name('s-asin')
+        print("total",len(data))
+        name=[]
+        price=[]
+        image=[]
+    
+        for data in driver.find_elements_by_class_name('s-asin'):
+            if data.find_element_by_class_name('a-size-medium ') and data.find_element_by_class_name('a-price-whole'):
+                    
+                n=data.find_element_by_class_name('a-size-medium ')
+                n=n.text
+                #l=n.split('-')
+                name.append(n)
+                #color.append(l[1])
+                p=data.find_element_by_class_name('a-price-whole')
+                price.append(p.text)
+                img=data.find_element_by_tag_name('img').get_attribute('src')
+                image.append(img)
+
+                #print("data======",name.text,"price",price.text,"image",img)
+            else:
+                print("data sorted 1")
+        df=pd.DataFrame({"name":name,"price":price,"img":img})
+        print(df)
+        df.to_csv('myfile3.csv')
+        print("----------------------------end====================")
+        driver.close() 
+    return render(request,'index.html')
 
 
-   # item_to_search = "apple mobile"
-   # search_box = driver.find_element_by_id('twotabsearchtextbox')
-    #search_box.clear()
-    #search_box.send_keys(item_to_search)
-    #search_box.send_keys(Keys.RETURN)
-    #driver.close() 
-    time.sleep(10)
+
+
+
+
+
+#    # item_to_search = "apple mobile"
+#    # search_box = driver.find_element_by_id('twotabsearchtextbox')
+#     #search_box.clear()
+#     #search_box.send_keys(item_to_search)
+#     #search_box.send_keys(Keys.RETURN)
+#     #driver.close() 
+#     time.sleep(10)
 
 
 
